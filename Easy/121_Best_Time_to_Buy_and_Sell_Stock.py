5db9596dd09a42b28c43ca8bc3992834ea5f5625
Uma Elangovan <umaelangovan06@gmail.com>
2026-02-15 10:48:47 +0530

    Completed_DC_121_Best_Time_to_Buy_and_Sell_Stock

--- Easy/121_Best_Time_to_Buy_and_Sell_Stock.py
+++ Easy/121_Best_Time_to_Buy_and_Sell_Stock.py
@@ -6,5 +6,25 @@ class Solution(object):
             min_price=min(price,min_price)
             max_profit=max(max_profit,price-min_price)
         return max_profit
-
-        
+    
+# class Solution(object):
+#     def maxProfit(self, prices):
+#         """
+#         :type prices: List[int]
+#         :rtype: int
+#         """
+#         buy=0
+#         sell=1
+#         profit=0
+#         count=0
+#         for i in prices:
+#             count+=1
+#         while sell<count:
+#             if prices[sell]>prices[buy]:
+#                 cur_profit=prices[sell]-prices[buy]
+#                 if cur_profit>profit:
+#                     profit=cur_profit
+#             else:
+#                 buy=sell
+#             sell+=1
+#         return profit
